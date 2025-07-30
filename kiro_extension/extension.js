974a0eb82216e0c0eb70bde76841078e9189620c
komiyamma <komiyama0629@gmail.com>
2025-07-30 22:46:15 +0900

    秀丸からkiroを寄贈。そのファイルが属しているリポジトリをワーキングディレクトリにして起動する。カーソルの位置に従う。カーソルが画面外なら秀丸で映ってる画面に近い位置で起動する。

---
+++ kiro_extension/extension.js
@@ -0,0 +1,35 @@
+const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
+
+// 一時フォルダのパス (OSによって異なるので注意)
+const tempDir = process.env.TEMP || process.env.TMP;
+
+// ファイル名
+const fileName = 'HmOpenKiroFromHidemaru.txt';
+
+// ファイルのフルパス
+const fileFullPath = path.join(tempDir, fileName);
+
+function activate(context) {
+
+    // ファイルの存在を確認し、存在すれば削除する
+    fs.access(fileFullPath, fs.constants.F_OK, (err) => {
+        if (err) {
+            ; // ファイルは存在しない
+        } else {
+            // ソースリポジトリの表示
+            vscode.commands.executeCommand('workbench.view.scm');
+
+            // ファイルが存在する場合は削除する
+            fs.unlink(fileFullPath, (err) => { });
+         }
+    });
+}
+
+function deactivate() {}
+
+module.exports = {
+    activate,
+    deactivate
+}
